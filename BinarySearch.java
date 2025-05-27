b73da67688b19f9e9866c80a42561daa9cac4574
yeexiong17 <wyeexiong17@gmail.com>
2025-05-28 00:25:09 +0800

    feat: binary search in java

---
+++ BinarySearch.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class BinarySearch {
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] == target) {
+                return mid;
+            } else if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }  
+
+    public static void main(String[] args) {
+
+        // implement array to store the data
+        String[] dataArray = new String[1000];
+        int i = 0;
+        try {
+            File myObj = new File("dataset_sample_1000.csv");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                dataArray[i] = data;
+                i++;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        int result = binarySearch(dataArray, "1000");
+        System.out.println(result);
+    }  
+}
